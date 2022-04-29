d07b168f7fdca9980701af9b4013f5c3a475b640
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2022-04-29 23:00:39 +0200

    Add files via upload

---
+++ pages/blog.php
@@ -0,0 +1,156 @@
+<?php
+
+	include("../resources/PHP/Header.inc.php");
+	include("../resources/PHP/Class.Shop.php");
+	include("../core/Cryptography.php");
+	
+	$shop  		  = new Shop();
+	$cryptography = new Cryptography();
+	
+	$token = $cryptography->getToken();
+	$_SESSION['token'] = $token;
+	
+	if(isset($_GET['cat'])) {
+		$cat = (int) $_GET['cat'];
+	}
+
+	if(isset($_GET['blogid'])) {
+		$blogid = (int) $_GET['blogid'];
+	}
+	
+	if(isset($_GET['page'])) {
+		$page = (int) $_GET['page'];
+		} else {
+		$page = 1;
+	}
+	
+	$baseurl = $shop->getbase();
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=0.73">
+	
+<?php
+	echo $shop->getmeta("../server/config/site.conf.json");				
+?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo $baseurl;?>resources/style/pages.css">
+
+</head>
+
+<body>
+
+<?php
+include("../resources/PHP/Header.php");
+?>
+
+<div id="wrapper">
+
+	<h1>Blog</h1>
+		
+	<div id="ts-shop-page"></div>
+<?php
+
+	$json = "../inventory/blog.json";
+	$pagelist = $shop->getpagelist($json,'blog');
+				
+	$num = count($pagelist);
+	
+	if($num >=1) {
+		
+	foreach($pagelist as $row)	{
+		
+		if(isset($blogid) >=1) {
+			
+			if($row['blog.id'] == $blogid && $row['blog.status'] == 1) {
+			?>
+				<div class="ts-shop-page-item">
+					<div class="ts-shop-page-item-header-focus">
+					<?php
+					if(strlen($row['blog.image.header']) > 30) {
+						echo '<img src="'.str_replace('../','',$baseurl.$shop->cleanInput($row['blog.image.header'])).'" />';
+					}
+					?>
+					</div>
+					<div class="ts-shop-page-item-main">
+						<div class="ts-shop-page-item-title">
+							<h1><?php echo $shop->cleanInput($row['blog.title']);?></h1>
+						</div>
+						<div class="ts-shop-page-item-titles">
+						
+						<?php 
+						
+						if($row['blog.handle'] != '') {
+							echo '<span class="ts-shop-page-item-author">By '.$shop->cleanInput($row['blog.handle']).'</span>';
+							} elseif($row['blog.author'] != '') { 
+							echo '<span class="ts-shop-page-item-author">By '.$shop->cleanInput($row['blog.author']).'</span>';
+						} else {}
+						
+						?>
+							<span class="ts-shop-page-item-date"><?php echo $shop->cleanInput($row['blog.published']);?></span>
+						</div>
+						<div class="ts-shop-page-item-textbox"><?php echo $shop->cleanpageoutput($row['blog.long.text']);?></div>
+						</div>
+						<div class="ts-shop-page-item-main-footer">
+							<span class="ts-shop-page-item-tags"><?php echo $shop->cleanInput($row['blog.tags']);?></span>
+						</div>
+					</div>	
+				</div>
+			<?php			
+				
+			}
+			
+		} else {
+	?>
+			<div class="ts-shop-page-item">
+				<div class="ts-shop-page-item-header">
+				<?php
+				if(strlen($row['blog.image.header']) > 30) {
+					echo '<img src="'.$shop->cleanInput($row['blog.image.header']).'" width="" height="" />';
+				}
+				?>
+				</div>
+				<div class="ts-shop-page-item-main">
+					<div class="ts-shop-page-item-title">
+						<h1><?php echo $shop->cleanInput($row['blog.title']);?></h1>
+					</div>
+					<div class="ts-shop-page-item-titles">
+						
+						<?php 
+						
+						if($row['blog.handle'] != '') {
+							echo '<span class="ts-shop-page-item-author">By '.$shop->cleanInput($row['blog.handle']).'</span>';
+							} elseif($row['blog.author'] != '') { 
+							echo '<span class="ts-shop-page-item-author">By '.$shop->cleanInput($row['blog.author']).'</span>';
+						} else {}
+						
+						?>
+		
+						<span class="ts-shop-page-item-date"><?php echo $shop->cleanInput($row['blog.published']);?></span>
+					</div>
+					<div class="ts-shop-page-item-textbox"><?php echo $shop->cleanInput($row['blog.short.text']);?></div>
+					</div>
+					<div class="ts-shop-page-item-main-footer">
+						<span class="ts-shop-page-item-rm"><a href="<?php echo (int)$shop->cleanInput($row['blog.id']);?>/<?php echo $shop->seoUrl($shop->cleanInput($row['blog.title']));?>/">read more &raquo;</a></span> 
+						<span class="ts-shop-page-item-tags"><?php echo $shop->cleanInput($row['blog.tags']);?></span>
+					</div>
+				</div>	
+			</div>
+
+		<?php
+			}
+		}
+	} else {
+		echo "No blogs have been written yet.";
+	}
+	
+	?>
+</div>
+
+<?php
+include("../resources/PHP/Footer.php");
+?>
+</body>
+</html>
