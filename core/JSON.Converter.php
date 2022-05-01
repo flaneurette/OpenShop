1eeaccfbac031b598949c349157e22bd0cc2bf3b
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2022-05-01 10:04:06 +0200

    Update JSON.Converter.php

--- core/JSON.Converter.php
+++ core/JSON.Converter.php
@@ -6,7 +6,7 @@ include_once("Backup.php");
 
 class Converter {
 	
-	CONST CSV					= "inventory/csv/";
+	CONST CSV				= "inventory/csv/";
 	CONST SERVERCSV				= "server/config/csv/";
 	CONST LOGGINGDIR 			= "server/logging/";
 	CONST BACKUPS				= "inventory/backups/";
@@ -14,7 +14,7 @@ class Converter {
 	CONST FILE_ENC				= "UTF-8";
 	CONST FILE_OS				= "WINDOWS-1252"; // only for JSON and CSV, not the server architecture.
 	CONST MAXINT  				= 9999999999;
-	CONST DEPTH					= 10024;
+	CONST DEPTH				= 10024;
 	
 	public function __construct($params = array()) 
 	{ 
