154d0f49a13cef5bd522d806bc6b447055696032
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2022-05-06 17:00:22 +0200

    Update Sessions.php

--- server/crontabs/Sessions.php
+++ server/crontabs/Sessions.php
@@ -3,8 +3,8 @@
 // Cron.php
 
 /* 
-  Add a crontab through a linux terminal (sudo crontab -e) and link to /var/www/html/cron.php to empty the /session/ folder.
-  !! this file is still experimental, and might not work on all instances. !!
+  Add a crontab through a linux terminal (sudo crontab -e) and link to /var/www/html/server/crontabs/sessions.php to empty the /session/ folder.
+  this file is still experimental, and might not work on all instances.
 */
 
 system("rm -f -R ../session/");
